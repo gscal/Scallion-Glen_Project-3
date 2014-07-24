5b2b27cbb1cf64b4113d02be930521778f57614e
Glen Scallion <gscal@fullsail.edu>
2014-07-24 13:08:25 -0500

    Json
    Getting to know how to create json documents

--- json.js
+++ json.js
@@ -1,9 +1,39 @@
-<!doctype html>
-<html>
+var json = {
+	"friends": {
+		"001435235":{
+			"name": "Chris",
+			"age": 30
+		},
+		"001435238":{
+			"name": "Scott",
+			"age": 42
+		},
+		"001435230":{
+			"name": "Jason",
+			"age": 32
+	}
+};
+for (var key in json.friends) {
+	var friend = json.friends[key];
+	//friend.age
+};
 
-	<head>
-		<meta charset="utf-8" />
-		<>Data Structures</title>
-	</head>
-
-	<body>
+var json2 = {
+	"friends": [
+		{
+			"uuid": "001435235",
+			"name": "Chris"
+			"age" : 30
+		},
+		{
+			"uuid": "001435235",
+			"name": "Scott"
+			"age" : 42
+		},
+		{
+			"uuid": "001435235",
+			"name": "Jason"
+			"age" : 32
+		}
+	]
+}
